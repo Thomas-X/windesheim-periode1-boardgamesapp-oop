05b12248ee59c4b3598f4e8c7562aab72ddb40ac
Thomas Zwarts <thomaszwarts@gmail.com>
2018-10-27 23:18:34 +0200

    misc

--- src/lib/CMailer.php
+++ src/lib/CMailer.php
@@ -57,7 +57,8 @@ class CMailer
         }
         $pyFile = __DIR__ . '/python/main.py';
         try {
-            dd("{$_ENV['PYTHON3_NAME']} {$pyFile} \"{$this->mail['to']}\" \"{$this->mail['subject']}\" \"{$this->mail['body']}\" \"{$_ENV['GMAIL_EMAIL']}\" \"{$_ENV['GMAIL_PASSWORD']}\"");
+            echo ("{$_ENV['PYTHON3_NAME']} {$pyFile} \"{$this->mail['to']}\" \"{$this->mail['subject']}\" \"{$this->mail['body']}\" \"{$_ENV['GMAIL_EMAIL']}\" \"{$_ENV['GMAIL_PASSWORD']}\"");
+            die;
             shell_exec("{$_ENV['PYTHON3_NAME']} {$pyFile} \"{$this->mail['to']}\" \"{$this->mail['subject']}\" \"{$this->mail['body']}\" \"{$_ENV['GMAIL_EMAIL']}\" \"{$_ENV['GMAIL_PASSWORD']}\"");
         } catch (\Exception $exception) {
             var_dump($exception);
