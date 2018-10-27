a3c2ec4fca5fa8e81e0503cb14413de547101cab
Thomas Zwarts <thomaszwarts@gmail.com>
2018-10-27 21:58:51 +0200

    misc

--- resources/views/layouts/app.php
+++ resources/views/layouts/app.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <base href="https://adsd.clow.nl/~s1130146/P1_OOAPP_Opdracht"/>
+    <base href="https://adsd.clow.nl/~s1130146/P1_OOAPP_Opdracht/"/>
 
     <!-- Roboto font -->
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
@@ -14,6 +14,7 @@
 	 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link href="/css/app.css" rel="stylesheet"/>
+    <link href="css/app.css" rel="stylesheet"/>
 
     <title>Boardgamesapp - <?= $title ?></title>
 </head>
